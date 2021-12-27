4d50a82003ac80f85cd8434c321b0c5ed5210014
Justin Samuel <justin@codzoc.com>
2021-12-27 13:21:07 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,35 +1,59 @@
-rm -rf var/*
-rm -rf pub/static/*
-rm -rf generated/*
-chmod 777 var -R
-chmod 777 pub/static -R
-chmod 777 pub/media -R
-chmod 777 generated -R
+sudo rm -rf var/*
+sudo rm -rf pub/static/* 
+sudo rm -rf generated/*
+sudo chmod 777 var -R
+sudo chmod 777 pub/static -R
+sudo chmod 777 pub/media -R
+sudo chmod 777 generated -R
 echo "Cache folders removed... \n"
 
 if [ "$1" = "-u" ]
 	then
-	/opt/lampp/bin/php bin/magento setup:upgrade
-	chmod 777 var -R
-	chmod 777 pub/static -R
-	chmod 777 pub/media -R
+	sudo rm -rf app/code/Mgt/Varnish/etc/module.xml
+	sudo rm -rf app/code/Mgt/Varnish/registration.php
+	sudo /opt/lampp/bin/php -dmemory_limit=5G bin/magento setup:upgrade
+	sudo chmod 777 var -R
+	sudo chmod 777 pub/static -R
+	sudo chmod 777 pub/media -R
 	echo "Setup Upgraded... \n"
+	notify-send "Setup Upgraded" "Upgraded Magento Schema"
 fi
 
 if [ "$1" = "-f" ]
 	then
-	/opt/lampp/bin/php bin/magento cache:flush
+	sudo /opt/lampp/bin/php bin/magento cache:flush
 	echo "\nCache Flushed... \n"
+	notify-send "Cache Flushed" "Cleared Magento Cache"
 fi
 
 if [ "$1" = "-d" ]
 	then
-	/opt/lampp/bin/php bin/magento setup:static-content:deploy en_US en_GB -f
+	/opt/lampp/bin/php -dmemory_limit=5G bin/magento setup:static-content:deploy en_US en_GB fr_FR -f
+	notify-send "Deployed" "Deployed static-content"
 fi
 
-chmod 777 var -R
-chmod 777 pub/static -R
-chmod 777 pub/media -R
-chmod 777 generated -R
+if [ "$1" = "-dg" ]
+	then
+	/opt/lampp/bin/php -dmemory_limit=5G bin/magento setup:static-content:deploy en_GB -f
+	notify-send "Deployed" "Deployed static-content"
+fi
+
+if [ "$1" = "-df" ]
+	then
+	/opt/lampp/bin/php -dmemory_limit=5G bin/magento setup:static-content:deploy fr_FR -f
+	notify-send "Deployed FR" "Deployed FR static-content"
+
+fi
+
+if [ "$1" = "-i" ]
+	then
+	/opt/lampp/bin/php bin/magento indexer:reindex
+	notify-send "Reindexed" "Re-indexed site data"
+
+fi
+
+sudo chmod 777 var -R
+sudo chmod 777 pub/static -R
+sudo chmod 777 pub/media -R
+sudo chmod 777 generated -R
 echo "Clean up complete! \n"
-notify-send "Its Clean!" "Cleaned up magento"
